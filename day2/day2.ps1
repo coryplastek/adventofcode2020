94c227d532c5af4cf88249d1e1ff0db7f9f0c6fb
Cory Plastek <cory.plastek@gmail.com>
2020-12-02 01:29:59 -0600

    Day 2 single loop

--- day2/day2.ps1
+++ day2/day2.ps1
@@ -6,29 +6,25 @@ param(
 $passwords = Get-Content -Path $Path
 Write-Verbose "$Path has $($passwords.Count) passwords"
 
-$validPasswords = New-Object -TypeName System.Collections.Generic.List[String]
+$validPasswordsPart1 = New-Object -TypeName System.Collections.Generic.List[String]
+$validPasswordsPart2 = New-Object -TypeName System.Collections.Generic.List[String]
 
 foreach( $line in $passwords ) {
     $rep, $letter, $password = $line -split ':? '
     $minrep, $maxrep = $rep -split '-'
+    $firstIndex = $minrep
+    $secondIndex = $maxrep
 
+    # part 1:
     # password is valid if there are between $minrep and $maxrep (inclusive)
     # repitiions of the given letter in the password
     $letterCount = ($password.ToCharArray() -match $letter).Count
 
     if( $letterCount -ge $minrep -and $letterCount -le $maxrep ) {
-        $validPasswords.Add($password)
+        $validPasswordsPart1.Add($password)
     }
-}
 
-Write-Host ("Part 1: There are {0} valid passwords in {1} total passwords" -f $validPasswords.Count, $passwords.Count)
-
-$validPasswords = New-Object -TypeName System.Collections.Generic.List[String]
-
-foreach( $line in $passwords ) {
-    $rep, $letter, $password = $line -split ':? '
-    $firstIndex, $secondIndex = $rep -split '-'
-
+    # part 2:
     # password is valid if the given letter is in only one of the 1-based indexes
     # provided. if it is present multiple times, the password is not valid
     $firstChar = $password[$firstIndex-1]
@@ -36,8 +32,9 @@ foreach( $line in $passwords ) {
     $letterCount = ( ($firstChar + $secondChar).ToCharArray() -match $letter).Count
 
     if( ( ($firstChar -eq $letter) -or ($secondChar -eq $letter) ) -and ( $letterCount -eq 1 ) ) {
-        $validPasswords.Add($password)
+        $validPasswordsPart2.Add($password)
     }
 }
 
-Write-Host ("Part 2: There are {0} valid passwords in {1} total passwords" -f $validPasswords.Count, $passwords.Count)
+Write-Host ("Part 1: There are {0} valid passwords in {1} total passwords" -f $validPasswordsPart1.Count, $passwords.Count)
+Write-Host ("Part 2: There are {0} valid passwords in {1} total passwords" -f $validPasswordsPart2.Count, $passwords.Count)
